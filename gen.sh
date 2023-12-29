8bcf805184ac1a10f5081fad697fabde9d62a7e6
Chang Yang <yangchang@deepin.org>
2023-12-29 18:16:24 +0800

    add profile support

--- gen.sh
+++ gen.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+ISOARCH=x86_64
+PROFILE=liveimage-minimal
+
+ISOFILE=eweos-$ISOARCH-$PROFILE.iso
+
 if [ ! -f "pacman.ewe.conf" ]; then
   wget https://raw.githubusercontent.com/eweOS/packages/pacman/pacman.conf -O pacman.ewe.conf
+  sed -i "s/Architecture = auto/Architecture = $ISOARCH/g" pacman.ewe.conf
 fi
 
 function crsh(){
@@ -21,11 +27,11 @@ mkfs.fat -F 32 ./bootfs.img
 sudo mkdir -p ./rootfs/boot
 sudo mount ./bootfs.img ./rootfs/boot
 
-sudo pacstrap -G -M -C ./pacman.ewe.conf ./rootfs base greetd tinyramfs linux efistub-tools neofetch vim sudo dinit
+sudo pacstrap -G -M -C ./pacman.ewe.conf ./rootfs `cat profiles/$PROFILE/packages.txt | xargs`
 
-cp ./configure.sh ./rootfs/configure.sh && chmod +x ./rootfs/configure.sh
+cp ./profiles/$PROFILE/config.sh ./rootfs/config.sh && chmod +x ./rootfs/config.sh
 
-crsh "/configure.sh"
+crsh "/config.sh"
 
 sudo umount ./rootfs/boot
 
@@ -34,9 +40,9 @@ sudo cp ./bootfs.img ./isofs/efi.img
 sudo mksquashfs ./rootfs ./isofs/root.sfs
 
 sudo xorriso -as mkisofs \
-   -o eweos.iso \
+   -o $ISOFILE \
    -R -J -v -d -N \
-   -x eweos.iso \
+   -x $ISOFILE \
    -hide-rr-moved \
    -no-emul-boot \
    -eltorito-platform efi \
@@ -44,3 +50,5 @@ sudo xorriso -as mkisofs \
    -V "EWE_ISO" \
    -A "eweOS Live ISO"  \
    isofs
+
+sha256sum $ISOFILE > $ISOFILE.sha256
