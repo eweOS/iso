b1f6b3a3c98cdadfca40d8122c463b5b045343bf
Yao Zi <ziyao@disroot.org>
2025-04-24 09:46:21 +0000

    Adjust path of labwc configuration to fit in xfce4-session changes
    Signed-off-by: Yao Zi <ziyao@disroot.org>

--- profiles/liveimage-desktop-xfce/subprofiles/10-desktop-xfce/config.sh
+++ profiles/liveimage-desktop-xfce/subprofiles/10-desktop-xfce/config.sh
@@ -13,16 +13,19 @@ command = "/usr/local/bin/live-intro"
 user = "ewe"
 EOF
 
-sudo -u ewe mkdir -p /home/ewe/.config/labwc
+sudo -u ewe mkdir -p /home/ewe/.config/xfce4/labwc
 
-sudo -u ewe cat <<EOF >> /home/ewe/.config/labwc/autostart
+sudo -u ewe cat <<EOF >> /home/ewe/.config/xfce4/labwc/autostart
 activate-linux "Live Mode" "All changes will be lost after reboot" &
 xfce4-terminal -e /usr/local/bin/live-intro &
 xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus &
 for f in ~/Desktop/*.desktop; do chmod +x "\$f"; gio set -t string "\$f" metadata::xfce-exe-checksum "\$(sha256sum "\$f" | awk '{print \$1}')"; done
 EOF
 
-sudo -u ewe cat <<EOF >> /home/ewe/.config/labwc/environment
+sudo -u ewe cp /usr/share/xfce4/labwc/labwc-environment \
+	/home/ewe/.config/xfce4/labwc/environment
+
+sudo -u ewe cat <<EOF >> /home/ewe/.config/xfce4/labwc/environment
 # disable hw cursor (for VM)
 WLR_NO_HARDWARE_CURSORS=1
 # set gtk4 renderer (for VM)
